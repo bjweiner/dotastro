0c0f36b735c74056bb46d6c9db988c1ddb7f888b
Benjamin Weiner <bjweiner@gmail.com>
2018-09-27 00:28:49 -0400

    test making a package exercise

---
+++ packaging/package_test/code.py
@@ -0,0 +1,5 @@
+
+def do_something(something):
+    if something == '':
+        something = 'default'
+    print(something)# this will make it much easier in future problems to see that something is actually happening
